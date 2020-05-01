b2fcb908ebf77390696f63a3b2faee3797101191
AaronMullan <aaroncmullan@gmail.com>
2020-05-01 15:46:15 -0700

    crude lemmy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import lemmy from './lemmy.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={lemmy} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Shave Dave 2020
         </p>
         <a
           className="App-link"
