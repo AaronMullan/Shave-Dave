fd6224f3e58feb1966e4568ddc3bf04332163452
AaronMullan <aaroncmullan@gmail.com>
2020-05-12 15:01:25 -0700

    removed unneeded file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import Matchup from "./matchup.js";
-import MatchupModal from "./matchup-modal.js"
 import Final from "./matchup-final.js";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
