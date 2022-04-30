c99c19f696d71ee70181357770c3929d9b87a673
danzhaas <dan.z.haas@gmail.com>
2022-04-30 13:54:59 -0400

    in progress - adds logos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import logo from "./pix/2022-logo-fullscreen.JPG";
+import daveLogo from "./pix/2022-logo-fullscreen.JPG";
+import odesaLogo from "./pix/odesa-logo.jpg";
+import hlmsLogo from "./pix/HLMS-small-logo.jpg";
+import wckLogo from "./pix/world-central-kitchen-logo.JPG";
+// import odesaLogo from "%PUBLIC_URL%/assets/odesa-logo.jpg";
 import AboutPage from "./about-page.js";
 
 function App() {
@@ -9,7 +13,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img
-          src={logo}
+          src={daveLogo}
           alt="Shave Dave Covid-19 Relief"
           className="Header-image"
         />
@@ -22,6 +26,11 @@ function App() {
             <h2>In Person at <b>The 4500 Lot</b> at Hamilton-Lauraville Main Street, Baltimore, MD, and <b>livestreamed</b> here and on social media</h2>
             <br></br>
             <h3>Join us for our 7th annual charity fund-raising show including live music and entertainment. At the end, we will SHAVE DAVE.  All proceeds go to Baltimore Sister City Odessa, the World Central Kitchen, and Hamilton-Lauraville Main Street.</h3>
+            <div className="charity-logo-grp">
+              <a id="odesa" href="https://www.baltimoreodesa.org/" target="_blank"><img className="charity-logo" src={odesaLogo} alt="Baltimore Sister City Odesa Logo" /></a>
+              <a id="hlms" href="https://www.bmoremainstreet.com/home" target="_blank"><img className="charity-logo" src={hlmsLogo} alt="Hamilton Lauraville Main Street Logo" /></a>
+              <a id="wck" href="https://wck.org/" target="_blank"><img className="charity-logo" src={wckLogo} alt="World Central Kitchen Logo" /></a>
+            </div>
             <br></br>
             <h1>#shavedave2022</h1>
           </div>
