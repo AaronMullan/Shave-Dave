96449470c537bd9492465548e25cfbf74a2427b5
danzhaas <dan.z.haas@gmail.com>
2022-05-05 19:36:25 -0400

    adds email box

---
+++ src/utils/index.js
@@ -0,0 +1,28 @@
+export function sendEmail({updateEmail}) {
+    const address = document.querySelector("#email-input").value;
+
+    const url = 'https://shave-dave-server.herokuapp.com/api/emails/';
+
+    const email = {
+        address
+    };
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(email),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    fetch(url, options)
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            if (res.msg === "Email received") {
+                updateEmail("Good news, you're on our mailing list.  You'll hear from us soon.");
+            } else {
+                updateEmail("Something went wrong.  Try again.");
+            }
+        })
+}
