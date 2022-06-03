70524c65f68733823b32d108f4a6eab2f7bb5d01
danzhaas <dan.z.haas@gmail.com>
2022-06-02 23:50:20 -0400

    adds total raised

--- src/components/DonateView.js
+++ src/components/DonateView.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import BeardDashboard from "./BeardDashboard";
 import beardStyles from "../beards-array";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Spinner } from "react-bootstrap";
 
 export default function DonateView() {
     const [ isLoading, updateLoading ] = useState(true);
@@ -15,6 +16,9 @@ export default function DonateView() {
 
     function getBeardAmount (result, beardType) {
         return result.donations.filter(data => data.beardVote === beardType).map(data => data.amount).reduce((a, b) => a + b, 0);
+    }
+    function getTotalAmount () {
+        return amounts.anchor + amounts.crab + amounts.clean + amounts.dubStache;
     }
 
     useEffect(() => {
@@ -49,11 +53,25 @@ export default function DonateView() {
     return (
         <div id="donate-view">
             <h2>How should we Shave Dave?  Donate to vote.  Voting ends Saturday, June 25 at 5 PM!</h2>
-            <ul>
-                <li> Decide your favorite Beard and donate to that style.</li>
+            <ul id="instructions">
+                <li>Decide your favorite Beard and donate to that style.</li>
                 <li>Follow us on FB and Instagram to track the leader.</li>
-                <li>Tune in or show up to the Live Variety Show June 26 where Dave will be shaved LIVE.</li>
+                <li>Tune in or show up to the Live Variety Show June 25 where Dave will be shaved LIVE.</li>
             </ul>
+            <div id="total">
+                <h2>Total Raised: </h2>
+                { isLoading ? 
+                // <Spinner />
+                <div
+                    className="spinner-border spinner-border-lg text-info no-donate"
+                    role="status"
+                >
+                    <span className="visually-hidden"></span>
+                </div>
+                :
+                <h2><b>${getTotalAmount()}</b></h2>
+                }
+            </div>
             <br />
             <h3 id="notice">Check out this year's face-do's, donate to your favorite, and then join our mailing list for updates.</h3>
             <BeardDashboard beardsSortedByAmount={beardsSortedByAmount} isLoading={isLoading} />
