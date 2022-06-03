8211f046c97cbe8c1ea8e9687d4c8296e337a531
danzhaas <dan.z.haas@gmail.com>
2022-06-02 22:44:55 -0400

    updates acknowledgements

--- src/components/AboutView.js
+++ src/components/AboutView.js
@@ -22,18 +22,28 @@ export default function AboutView() {
             • We will host a Live Variety Show accepting pledges up until 5:00PM SHARP then we will tally the final pledges and Dave will be shaved LIVE. It’ll be a HOOT and all for a good cause.
           </p>
 
-          <p><b>Acknowledgements: </b>
-            All Love to those who dedicated their time and expertise:
-            Kerry Spence, editor, smarts and motivator bar-none.
-            Dan Moyer, graphic artist, creative genius and all-around great guy.
-            Bill Puller of Blue Spark Barbershop, The Official Barber of Shave Dave. Head sculptor extraordinaire and also great friend with good taste in music.
-            Dave Seel of the Baltimore Restaurant Relief Fund and positive force to reckon with.
-            Aaron Mullan, web designer and best bud.
-            Kate Rowe, marketing genius and the sunshine in my day.
-            Paul Hartman, idea man and generous soul.
-            Amy Winter Jones, ShaveDave OG and Red Bull pusher-lady.
-            The Cokers, Danny and Dana, Bandaloop’s OGs and keepers of the Bartenders flame.
-            All of Bartenders EXTENDED FAMILY for their encouragement, love, and support. </p>
+          <p><b>Acknowledgements (So Far): </b></p>
+          <ul>
+            <li>All Love to those who dedicated their time and expertise:</li>
+            <li>Kerry Spence, editor, smarts and motivator bar-none.</li>
+            <li>Stephen Spence, my brother from the same Mother and ever supportive get it done guy.</li>
+            <li>Dan Moyer, graphic artist, creative genius, all-around great guy, golfer, &amp; longtime friend.</li>
+            <li>Bill Puller of Blue Spark Barbershop, The Official Barber of Shave Dave. Head sculptor extraordinaire, also good friend with good taste in music.</li>
+            <li>Karina Mandell, Chair, Baltimore Odesa Sister City and ball of never-ending energy.</li>
+            <li>Daniel Doty, Executive Director, HLMS and resource of deep means.</li>
+            <li>Julie Lin, HLMS, Julie keeps us all straight and solves the problems!</li>
+            <li>Aaron Mullan, web designer and best bud.</li>
+            <li>Dan Z Haas, web designer, generous with his ideas &amp; his time. New bud.</li>
+            <li>Max Bent, Creative genius, musician, and Live Show getter of things done and staying on track</li>
+            <li>Kate Rowe, marketing genius and the sunshine in my day.</li>
+            <li>Paul Hartman, idea man and generous soul.</li>
+            <li>Amy Winter Jones, ShaveDave OG and Red Bull pusher-lady.</li>
+            <li>The Cokers, Danny and Dana, Bandaloop’s OGs and keepers of the Bartenders flame.</li>
+            <li>Key Brewing Company – Mark, Mike, Jef, Mike Kulnich, Victor, Earl, Noah, Ethan.</li>
+            <li>Frank, Franks MDBBQ, MD_BBQI, tireless smoker of delicious meats, creator of Franksgiving, dedicated to service.</li>
+            <li>Angi &amp; Bobby Jordan, FastSigns, Joppa, MD. Enthusiastic collaborator, good friends with a we can make it happen attitude.</li>
+            <li>All of Bartenders EXTENDED FAMILY for their encouragement, love, and support.</li>
+          </ul>
           <p><b>ABOUT: ShaveDave for Ukraine Relief</b><br /><br />
             Shave Dave was created in 2011 by Dave Spence and Paul Hartman to raise money for Tsunami Relief in Japan. We subsequently ran several successful fundraisers for The National MS Society until Dave retired from the bar business in 2015. The COVID-19 Virus is forced the beard out of retirement in 2020. This year we will be raising money for the non-profit Baltimore Sister City Odesa to provide assistance to citizens of Ukraine so adversely affected by the crisis there. We will also be donating money to Hamilton-Lauraville Main Street. This year will be in person at The 4500 Lot at Hamilton-Lauraville Main Street and livestreamed here and on other social media platforms. The shave will be held on Saturday, June 25, 2022.
           </p>
