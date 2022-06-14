9bed66f96d0a8b737a1daa75f34c42b0fc80fb76
danzhaas <dan.z.haas@gmail.com>
2022-06-14 00:23:11 -0400

    adds img alt text and link rels

--- src/components/SponsorsView.js
+++ src/components/SponsorsView.js
@@ -14,38 +14,38 @@ export default function EventView() {
             <h2>Sponsors</h2>
             <div id="sponsor-logos">
                 <div className="col-12 col-sm-6 col-lg-4">
-                    <a href="https://bluesparkbarbershop.com/" target="_blank">
-                        <img src={blueSparkBarbershop} />
+                    <a href="https://bluesparkbarbershop.com/" target="_blank" rel="noopener noreferrer" >
+                        <img src={blueSparkBarbershop} alt="blue spark barbershop logo"/>
                     </a>
                 </div>
                 <div className="col-12 col-sm-6 col-lg-4">
-                    <a href="https://casafiglia.com/" target="_blank">
-                        <img src={casaFiglia} />
+                    <a href="https://casafiglia.com/" target="_blank" rel="noopener noreferrer" >
+                        <img src={casaFiglia} alt="casa figlia logo" />
                     </a>
                 </div>
                 <div className="col-12 col-sm-6 col-lg-4">
-                    <a href="https://www.fastsigns.com/downtown-baltimore-md/" target="_blank">
-                        <img src={fastSigns} />
+                    <a href="https://www.fastsigns.com/downtown-baltimore-md/" target="_blank" rel="noopener noreferrer" >
+                        <img src={fastSigns} alt="fast signs logo" />
                     </a>
                 </div>
                 <div className="col-12 col-sm-6 col-lg-4">
-                    <a href="https://franksmdbbq.com/" target="_blank">
-                        <img src={franksBbq} />
+                    <a href="https://franksmdbbq.com/" target="_blank" rel="noopener noreferrer" >
+                        <img src={franksBbq} alt="franks bbq logo" />
                     </a>
                 </div>
                 <div className="col-12 col-sm-6 col-lg-4">
-                    <a href="https://keybrewing.com/" target="_blank">
-                        <img src={keyBrewing} />
+                    <a href="https://keybrewing.com/" target="_blank" rel="noopener noreferrer" >
+                        <img src={keyBrewing} alt="key brewing logo" />
                     </a>
                 </div>
                 <div className="col-12 col-sm-6 col-lg-4">
-                    {/* <a href="" target="_blank"> */}
-                        <img src={planLogic} />
+                    {/* <a href="" target="_blank" rel="noopener noreferrer" > */}
+                        <img src={planLogic} alt="plan logic logo" />
                     {/* </a> */}
                 </div>
                 <div className="col-12 col-sm-6 col-lg-4">
-                    <a href="https://www.tortugakombucha.com/" target="_blank">
-                        <img src={tortugaKombucha} />
+                    <a href="https://www.tortugakombucha.com/" target="_blank" rel="noopener noreferrer" >
+                        <img src={tortugaKombucha} alt="tortuga kombucha logo" />
                     </a>
                 </div>
             </div>
